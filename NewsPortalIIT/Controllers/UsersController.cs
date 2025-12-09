2937d7ee1fa0abfa6ebb3d3163d445a94996b713
Tayabur Rahman <tayabur.rahman@niftycoders.com>
2025-12-09 20:14:14 +0600

    user controller addded

--- NewsPortalIIT/Controllers/UsersController.cs
+++ NewsPortalIIT/Controllers/UsersController.cs
@@ -10,27 +10,60 @@ public class UsersController : BaseController
     [HttpGet]
     public IEnumerable<User> Get()
     {
-        return new List<User> { new User() };
+        var data = GetDbData();
+        return data.Users ?? new List<User>();
     }
 
     [HttpGet("{id}")]
-    public User Get(int id)
+    public User? Get(int id)
     {
-        return new User();
+        var data = GetDbData();
+        return data.Users?.FirstOrDefault(n => n.Id == id);
     }
 
     [HttpPost]
     public void Post([FromBody] User model)
     {
+        var data = GetDbData();
+
+        if (data.Users == null) data.Users = new List<User>();
+
+        // Generate ID
+        int userId = data.Users.Count != 0 ? data.Users.Max(n => n.Id) + 1 : 1;
+        model.Id = userId;
+
+        data.Users.Add(model);
+
+        SaveDbData(data);
     }
 
     [HttpPut("{id}")]
     public void Put(int id, [FromBody] User model)
     {
+        var data = GetDbData();
+        if (data.Users == null) return;
+
+        var existingUser = data.Users.FirstOrDefault(n => n.Id == id);
+        if (existingUser != null)
+        {
+            existingUser.Name = model.Name;
+            existingUser.Email = model.Email;
+
+            SaveDbData(data);
+        }
     }
 
     [HttpDelete("{id}")]
     public void Delete(int id)
     {
+        var data = GetDbData();
+        if (data.Users == null) return;
+
+        var userToDelete = data.Users.FirstOrDefault(n => n.Id == id);
+        if (userToDelete != null)
+        {
+            data.Users.Remove(userToDelete);
+            SaveDbData(data);
+        }
     }
 }
